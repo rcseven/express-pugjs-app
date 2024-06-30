5a60155db62867114f0694603e55b89b2e903cd1
Ryan Edquila <rcedquila@gmail.com>
2024-06-30 19:13:52 +0800

    Update app.js to remove unnecessary console.log statement and fix cookie options

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,10 +18,8 @@ const createSendToken = (user, statusCode, req, res) => {
         expires: new Date(
             Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         ),
-        path: '/',
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'Lax'
+        secure: process.env.NODE_ENV === 'production'
     };
 
     res.cookie('jwt', token, cookieOptions);
@@ -63,6 +61,15 @@ exports.login = catchAsync(async (req, res, next) => {
     createSendToken(user, 200, req, res);
 });
 
+exports.logout = (req, res) => {
+    res.cookie('jwt', 'loggedout', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+
+    res.status(200).json({ status: 'success' });
+};
+
 exports.updatePassword = catchAsync(async (req, res, next) => {
     // 1) Get user from collection
     const user = await User.findById(req.user.id || req.user._id).select(
@@ -93,6 +100,8 @@ exports.protect = catchAsync(async (req, res, next) => {
         req.headers.authorization.startsWith('Bearer')
     ) {
         token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
     }
 
     if (!token) {
@@ -130,9 +139,40 @@ exports.protect = catchAsync(async (req, res, next) => {
 
     // GRANT ACCESS TO PROTECTED ROUTE
     req.user = currentUser;
-
+    res.locals.user = currentUser;
     next();
 });
+
+// Onyl for rendered pages, no errors!
+exports.isLoggedIn = async (req, res, next) => {
+    if (req.cookies.jwt) {
+        try {
+            // 1) Verification token
+            const decoded = await promisify(jwt.verify)(
+                req.cookies.jwt,
+                process.env.JWT_SECRET
+            );
+
+            // 2) Check if user still exists cases if user deleted account but token is still valid
+            const currentUser = await User.findById(decoded.id);
+            if (!currentUser) {
+                return next();
+            }
+
+            // 3) Check if user changed password after the token was issued
+            if (currentUser.changedPasswordAfter(decoded.iat)) {
+                return next();
+            }
+
+            // THERE IS A LOGGED IN USER
+            res.locals.user = currentUser;
+            return next();
+        } catch (err) {
+            return next();
+        }
+    }
+    next();
+};
 
 exports.restrictTo = (...roles) => {
     return (req, res, next) => {
